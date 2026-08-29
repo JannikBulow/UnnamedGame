db21fa72e6ecc37d1aaeed40d3a1bfd7ff596538
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-08-30 00:18:29 +0200

    feat(asset-provider): add font struct

--- engine/include/engine/backend/asset_provider.h
+++ engine/include/engine/backend/asset_provider.h
@@ -3,6 +3,8 @@
 #ifndef UNNAMEDGAME_ENGINE_BACKEND_ASSET_PROVIDER_H
 #define UNNAMEDGAME_ENGINE_BACKEND_ASSET_PROVIDER_H
 
+#include "engine/util/math.h"
+
 #include <cstdint>
 #include <cstddef>
 
@@ -20,12 +22,30 @@ namespace backend {
         const uint8_t* pixels;
     };
 
+    struct Glyph {
+        uint32_t codepoint; // unicode
+        int offsetX;
+        int offsetY;
+        int advanceX;
+        math::Rect atlasBounds;
+    };
+
+    struct Font {
+        int baseSize;
+        int glyphCount;
+        Image atlas;
+        Glyph* glyphs;
+    };
+
     class IAssetProvider {
     public:
         virtual ~IAssetProvider() = default;
 
         virtual Image loadImage(const char* path) = 0;
         virtual void unloadImage(Image image) = 0;
+
+        virtual Font loadFont(const char* path, int size) = 0;
+        virtual void unloadFont(Font font) = 0;
     };
 }
 
