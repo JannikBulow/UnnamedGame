3665359e3efd675fa0fcee45f2de42041ce131d9
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-08-29 16:53:55 +0200

    feat(opengl-shader-programs): add texture shader

--- engine/include/engine/backends/opengl/shader_programs.h
+++ engine/include/engine/backends/opengl/shader_programs.h
@@ -4,7 +4,7 @@
 #define UNNAMEDGAME_ENGINE_BACKENDS_OPENGL_SHADER_PROGRAMS_H
 
 namespace backend::shaders {
-    constexpr const char* Vertex = R"(
+    constexpr const char* ColorVertex = R"(
 #version 330 core
 
 layout(location = 0) in vec2 aPosition;
@@ -18,7 +18,7 @@ void main() {
 }
 )";
 
-    constexpr const char* Fragment = R"(
+    constexpr const char* ColorFragment = R"(
 #version 330 core
 
 uniform vec4 uColor;
@@ -27,6 +27,42 @@ out vec4 FragColor;
 
 void main() {
     FragColor = uColor;
+}
+)";
+
+    constexpr const char* TextureVertex = R"(
+#version 330 core
+
+layout(location = 0) in vec2 aPosition;
+layout(location = 1) in vec2 aUV;
+
+out vec2 vUV;
+
+uniform mat4 uProjection;
+uniform mat4 uView;
+uniform mat4 uModel;
+
+uniform mat3 uTextureTransform;
+
+void main() {
+    vUV = (uTextureTransform * vec3(aUV, 1.0)).xy;
+
+    gl_Position = uProjection * uView * uModel * vec4(aPosition, 0.0, 1.0);
+}
+)";
+
+    constexpr const char* TextureFragment = R"(
+#version 330 core
+
+in vec2 vUV;
+
+uniform sampler2D uTexture;
+uniform vec4 uColor;
+
+out vec4 FragColor;
+
+void main() {
+    FragColor = texture(uTexture, vUV) * uColor;
 }
 )";
 }
