0dda0479444c3a68de3f7939ebd910eb0f84b4c7
jannik <dodoakmishot@gmail.com>
2026-09-04 10:36:01 +0200

    feat(render): add camera declaration

---
+++ engine/include/engine/render/camera.h
@@ -0,0 +1,12 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#ifndef UNNAMEDGAME_ENGINE_RENDER_CAMERA_H
+#define UNNAMEDGAME_ENGINE_RENDER_CAMERA_H
+
+#include "engine/backend/renderer.h"
+
+namespace engine {
+    using Camera = backend::Camera2D;
+}
+
+#endif //UNNAMEDGAME_ENGINE_RENDER_CAMERA_H
